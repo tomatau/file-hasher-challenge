c478f8fedc7d8d0cd92395b020fb6b992acb790d
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2025-08-03 14:10:39 +0100

    split file into chunks while hashing

--- src/workers/sha-256.worker.ts
+++ src/workers/sha-256.worker.ts
@@ -1,18 +1,27 @@
 import { createSHA256 } from 'hash-wasm'
 import type { PayloadDone, PayloadWorking } from './worker.types'
 
+const CHUNK_SIZE = 64 * 1024 * 1024 // 64MB
+
 self.onmessage = async ({ data: { file } }: MessageEvent<{ file: File }>) => {
   if (!file) return
   const sha256 = await createSHA256()
+
+  let offset = 0
 
   self.postMessage({
     status: 'working',
     payload: { received: file },
   } satisfies PayloadWorking)
 
-  const buffer = await file.arrayBuffer()
-  const typedbuffer = new Uint8Array(buffer)
-  sha256.update(typedbuffer)
+  while (offset < file.size) {
+    const chunk = file.slice(offset, offset + CHUNK_SIZE)
+    const buffer = await chunk.arrayBuffer()
+    const typedbuffer = new Uint8Array(buffer)
+    sha256.update(typedbuffer)
+    offset += buffer.byteLength
+  }
+
   const hash = sha256.digest('hex')
 
   self.postMessage({
